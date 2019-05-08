dd2ba427e93965a83f8ab1294d31a25dd6b085b3
spliitzx <fsmitchm@gmail.com>
2019-05-08 14:14:28 +0800

    Add global dynamic state

--- app/utils/http/Controller.ts
+++ app/utils/http/Controller.ts
@@ -1,10 +1,14 @@
 import { Response } from '../util/Response';
+import { state } from '../util/State';
 
 export class Controller {
+
+    public state: any;
 
     private responseHandler: Response;
 
     constructor(socket: any) {
+        this.state = state.internal;
         this.responseHandler = new Response(socket);
     }
 
@@ -14,6 +18,14 @@ export class Controller {
 
     respondWithError(message?: any) : void {
         this.responseHandler.error(message);
+    }
+
+    setState(newObj: any) : void {
+        this.state = {
+            ...this.state,
+            newObj
+        };
+        state.internal = newObj;
     }
 
 }
