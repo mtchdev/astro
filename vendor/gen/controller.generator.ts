4d2a56bf5358176cfc90aba939fd175c4964f306
whenofficial <fsmitchm@gmail.com>
2019-05-13 13:49:08 +0800

    Add CLI generators

---
+++ vendor/gen/controller.generator.ts
@@ -0,0 +1,33 @@
+const ControllerTemplate = `import { Controller } from 'vendor/astro/http/Controller';
+
+export class ControllerName extends Controller {
+
+    constructor(data) {
+        super(data);
+    }
+
+}
+`;
+
+import fs from 'fs';
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+cli();
+
+function cli() : void {
+    rl.question('Controller Name: ', (ans: string) => generate(ControllerTemplate, ans));
+}
+
+async function generate(template: string, cliName: string) {
+    let temp = template.replace(/ControllerName/g, cliName);
+    await fs.writeFileSync('app/controllers/' + cliName + '.ts', temp);
+    setTimeout(() => {
+        console.log('Successfully created ' + cliName + '.ts');
+        rl.close();
+    }, 250);
+}
---
+++ vendor/gen/model.generator.ts
@@ -0,0 +1,40 @@
+const ControllerTemplate = `import { Model } from 'vendor/astro/http/Model';
+
+export class modelName extends Model {
+
+    constructor() {
+        super(tableName);
+
+        this.table = tableName; // DB table name
+        this.fillable = []; // Fillable DB columns
+        this.noReturn = []; // Data to not return on a SELECT * FROM request
+    }
+
+}
+`;
+
+import fs from 'fs';
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+cli();
+
+function cli() : void {
+    rl.question('Model Name: ', (name: string) => {
+        rl.question('DB Table: ', (table: string) => generate(ControllerTemplate, name, table));
+    });
+}
+
+async function generate(template: string, cliName: string, cliTable: string) {
+    let slug = template.replace(/modelName/g, cliName);
+    let temp = slug.replace(/tableName/g, "'" + cliTable + "'");
+    await fs.writeFileSync('app/models/' + cliName + '.ts', temp);
+    setTimeout(() => {
+        console.log('Successfully created ' + cliName + '.ts');
+        rl.close();
+    }, 250);
+}
