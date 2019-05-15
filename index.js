99f956e37f661096c77975f536cdff5aa2da0dd1
spliitzx <fsmitchm@gmail.com>
2019-05-15 13:25:00 +0800

    Update scripts

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * No builds created. Run `npm run build` to create a JS-supported build.
+ */
+
+console.log('Start failed because you haven\'t compiled your project yet.');
+console.log('Did you mean:');
+console.log('\nnpm run start:dev');
+console.log()
