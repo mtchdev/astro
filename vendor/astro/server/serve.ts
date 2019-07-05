82aff9ca52d3b330ac73abdbab5f0b2cbaaa856a
spliitzx <fsmitchm@gmail.com>
2019-07-05 03:21:25 +0100

    Log

--- vendor/astro/server/serve.ts
+++ vendor/astro/server/serve.ts
@@ -1,11 +1,18 @@
 import { AppConfig } from '../../../config/app.config';
+import { RouterConfig } from '../../../config/router.config';
+import { Logger } from '../util/Logger';
+
+const ip = require('ip');
 
 export var serve = {
     up: (express: any) => {
         if (typeof AppConfig.port !== 'number')
             throw new Error('Port is not a number.');
 
-        express.listen(AppConfig.port, () => console.info('Server running on port ' + AppConfig.port + '.'));
+        express.listen(AppConfig.port, () => Logger.log(`Server running on host ${ip.address()}:${AppConfig.port}`));
+        if (RouterConfig.log_requests) {
+            Logger.log('Router log mode enabled. All requests will be logged.');
+        }
     },
     halt: (instance: any) => {
         instance.close();
