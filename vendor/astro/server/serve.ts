9b4f04a6a1bb6b91b517eedd59855316873473f3
spliitzx <fsmitchm@gmail.com>
2019-07-05 04:26:54 +0100

    Add serve errors

--- vendor/astro/server/serve.ts
+++ vendor/astro/server/serve.ts
@@ -9,7 +9,18 @@ export var serve = {
         if (typeof AppConfig.port !== 'number')
             throw new Error('Port is not a number.');
 
-        express.listen(AppConfig.port, () => Logger.log(`Server running on host ${ip.address()}:${AppConfig.port}`));
+        express.listen(AppConfig.port, () => Logger.log(`Server running on host ${ip.address()}:${AppConfig.port}`)).on('error', (data) => {
+            switch (data.code) {
+                case "EADDRINUSE":
+                    Logger.log(`Port ${AppConfig.port} is already in use.`, 'error');
+                    break;
+                default:
+                    Logger.log('A fatal error occured while trying to create the server.', 'error');
+                    break;
+            }
+
+            process.exit();
+        });
         if (RouterConfig.log_requests) {
             Logger.log('Router log mode enabled. All requests will be logged.');
         }
