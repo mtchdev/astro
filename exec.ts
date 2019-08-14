3cc7f777265dc7bf0b5eb10c1ad82863120bc84f
whenofficial <fsmitchm@gmail.com>
2019-08-14 17:08:30 +0800

    Check if env exists

--- exec.ts
+++ exec.ts
@@ -1,6 +1,12 @@
 import { AppConfig } from './config/app.config';
 import { Logger } from './vendor/astro/util/Logger';
 import { serve } from './vendor/astro/server/serve';
+import { existsSync } from 'fs';
+
+if (!existsSync(__dirname + '\\.env')) {
+    Logger.log('Environment not found. Please rename .env.example to .env', 'error');
+    process.exit();
+}
 
 AppConfig.environment.process();
 
